a7ad7e4c98b4f0650f64315b63b8b73cf01ff241
Rekunova <ma.rekunova@gmail.com>
2020-02-11 22:57:29 +0200

    webpack init

---
+++ src/calc.js
@@ -0,0 +1,13 @@
+import {data} from '../data';
+
+const root = document.querySelector("#calc");
+const calc = document.createElement("div");
+
+// render position names 
+
+Object.keys(data).forEach(x => {     
+  const positionName = document.createElement("div");
+  positionName.innerText = x;
+  calc.appendChild(positionName);
+});
+root.appendChild(calc);
