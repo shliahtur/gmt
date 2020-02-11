b51ea7f9aa979ba444eaa26a0cfa57a7f68f23bb
v_shliahtur <shliahtur@gmail.com>
2020-02-11 18:19:14 +0200

    д

---
+++ js/calc.js
@@ -0,0 +1,214 @@
+const data = {
+  Developer: [
+    {
+      specialization: "Java or .NET",
+      level: [
+        {
+          Junior: 1000,
+          Middle: 2100,
+          Senior: 3400,
+          TL: 4500,
+          Architect: 5500
+        }
+      ]
+    },
+    {
+      specialization: "Scala",
+      level: [
+        {
+          Junior: 1200,
+          Middle: 2300,
+          Senior: 3600,
+          TL: 4500,
+          Architect: 5500
+        }
+      ]
+    },
+    {
+      specialization: "C++",
+      level: [
+        {
+          Junior: 1000,
+          Middle: 2000,
+          Senior: 3200,
+          TL: 4200,
+          Architect: 5000
+        }
+      ]
+    },
+    {
+      specialization: "FrontEnd",
+      level: [
+        {
+          Junior: 1500,
+          Middle: 2800,
+          Senior: 3700,
+          TL: 4500,
+          Architect: 5500
+        }
+      ]
+    },
+    {
+      specialization: "Node",
+      level: [
+        {
+          Junior: 1500,
+          Middle: 2800,
+          Senior: 3700,
+          TL: 4500,
+          Architect: 5500
+        }
+      ]
+    },
+    {
+      specialization: "Python",
+      level: [
+        {
+          Junior: 1000,
+          Middle: 2100,
+          Senior: 3400,
+          TL: 4500,
+          Architect: 5000
+        }
+      ]
+    },
+    {
+      specialization: "PHP",
+      level: [
+        {
+          Junior: 1200,
+          Middle: 2000,
+          Senior: 3300,
+          TL: 4000,
+          Architect: 4500
+        }
+      ]
+    },
+    {
+      specialization: "Ruby",
+      level: [
+        {
+          Junior: 1300,
+          Middle: 2500,
+          Senior: 3600,
+          TL: 4500,
+          Architect: 4800
+        }
+      ]
+    },
+    {
+      specialization: "DS",
+      level: [
+        {
+          Junior: 1800,
+          Middle: 3000,
+          Senior: 4500,
+          TL: 5000,
+          Architect: 6000
+        }
+      ]
+    },
+    {
+      specialization: "Mobile",
+      level: [
+        {
+          Junior: 1200,
+          Middle: 2000,
+          Senior: 3300,
+          Expert: 4400
+        }
+      ]
+    }
+  ],
+  DevOps: [
+    {
+      specialization: "DEVOPS",
+      level: [
+        {
+          Junior: 1000,
+          Middle: 2500,
+          Senior: 3700,
+          Expert: 4400
+        }
+      ]
+    }
+  ],
+  AQA: [
+    {
+      specialization: "AQA",
+      level: [
+        {
+          Junior: 900,
+          Middle: 2200,
+          Senior: 3500,
+          TL: 4300,
+          Architect: 4500
+        }
+      ]
+    }
+  ],
+  QA: [
+    {
+      specialization: "QA",
+      level: [
+        {
+          Junior: 900,
+          Middle: 1400,
+          Senior: 2600,
+          Leader: 3300
+        }
+      ]
+    }
+  ],
+  Designer: [
+    {
+      specialization: "UX Designer",
+      level: [
+        {
+          Junior: 1000,
+          Middle: 2000,
+          Senior: 3000,
+          Expert: 4000
+        }
+      ]
+    }
+  ],
+  BA: [
+    {
+      specialization: "Business Analyst",
+      level: [
+        {
+          Junior: 1200,
+          Middle: 2000,
+          Senior: 3500
+        }
+      ]
+    }
+  ],
+  PM: [
+    {
+      specialization: "Project manager",
+      level: [
+        {
+          Trainee: 800,
+          Junior: 1500,
+          Middle: 3500,
+          Senior: 4500,
+          Expert: 5000
+        }
+      ]
+    }
+  ]
+};
+
+const root = document.querySelector("#calc");
+const calc = document.createElement("div");
+
+// render position names 
+
+Object.keys(data).forEach(x => {     
+  const positionName = document.createElement("div");
+  positionName.innerText = x;
+  calc.appendChild(positionName);
+});
+root.appendChild(calc);
