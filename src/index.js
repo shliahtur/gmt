9f9d15e7faa43e721671170c3ec91d3526a42ad8
shliahtur <shliahtur@gmail.com>
2020-02-12 23:32:42 +0200

    slider added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+import Glide from '@glidejs/glide'
 import './styles/style.css';
+import "../node_modules/@glidejs/glide/dist/css/glide.core.min.css";
+import "../node_modules/@glidejs/glide/dist/css/glide.theme.min.css";
+
+const glideConfig = { 
+autoplay: true,
+duration: 3000
+}
+
+new Glide('.glide', glideConfig).mount()
+
+// $(".table-tab-wr").on("click", ".tab", function() {
+//     // Удаляем классы active
+//     $(".table-tab-wr .tab").removeClass('active');
+ 
+//     // Добавляем классы active
+//     $(this).addClass('active');
+// });
+// console.log(123)
