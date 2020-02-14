204d1d0534fb836f5e37744755b9e5734b72b389
rekunova <ma.rekunova@gmail.com>
2020-02-14 09:44:28 +0200

    morning

--- src/index.js
+++ src/index.js
@@ -7,29 +7,26 @@ import renderSlider from './scripts/slider';
 // Calc.renderCalc();
 // renderSlider();
 
-// const tabLinks = document.querySelectorAll(".tabs a");
-// const tabPanels = document.querySelectorAll(".tabs-panel");
+const tabLinks = document.querySelectorAll(".tabs a");
+const tabPanels = document.querySelectorAll(".tabs-panel");
 
-// for (let el of tabLinks) {
-//     el.addEventListener("click", e => {
-//         e.preventDefault();
+for (let el of tabLinks) {
+    el.addEventListener("click", e => {
+        e.preventDefault();
 
-//         document.querySelector(".tabs li.active").classList.remove("active");
-//         document.querySelector(".tabs-panel.active").classList.remove("active");
+        document.querySelector(".tabs li.active").classList.remove("active");
+        document.querySelector(".tabs-panel.active").classList.remove("active");
 
-//         const parentListItem = el.parentElement;
-//         parentListItem.classList.add("active");
-//         const index = [...parentListItem.parentElement.children].indexOf(parentListItem);
-
-//         const panel = [...tabPanels].filter(el => el.getAttribute("data-index") == index);
-//         panel[0].classList.add("active");
-//     });
-// }
+        const parentListItem = el.parentElement;
+        parentListItem.classList.add("active");
+        const index = [...parentListItem.parentElement.children].indexOf(parentListItem);
 
         const panel = [...tabPanels].filter(el => el.getAttribute("data-index") == index);
         panel[0].classList.add("active");
     });
 }
+
+
 
 var tableTexts = document.getElementsByClassName('table-text');
 
